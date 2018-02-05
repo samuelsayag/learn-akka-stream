0d9e51a282a1e4dc76cadc4d4972aaa594b73702
Samuel SAYAG <samuel.sayag@clicktale.com>
2018-02-05 08:31:25 +0200

    add display explanation to Runner

--- src/main/scala/sam/test/akka/stream/helper/Runner.scala
+++ src/main/scala/sam/test/akka/stream/helper/Runner.scala
@@ -1,21 +1,37 @@
 package sam.test.akka.stream.helper
 
-import akka.Done
 import akka.actor.ActorSystem
 import akka.stream.{ActorMaterializer, Materializer}
 
 import scala.concurrent.Future
+import scala.util.Try
 
 trait Runner[T] {
+
+  def explanation: Seq[String] = Seq()
+
+  private def buildExplanation = {
+    val frameSize = Try(explanation.map(_.length).max).
+      toOption.getOrElse(0)
+    val frame = Seq.fill(frameSize)("*").mkString("")
+
+    frame :: explanation.toList ::: List(frame)
+  }
+
+  def printExplanation =
+    println(buildExplanation.mkString("\n"))
+
 
   def toExec(m: Materializer): Future[T]
 
   def exec[O](f: (Materializer) => Future[T],
               toDo: Option[(T) => O]): Unit = {
 
-    implicit val system = ActorSystem("QuickStart")
+    implicit val system: ActorSystem = ActorSystem("QuickStart")
     val mat: Materializer = ActorMaterializer()
     val ec = system.dispatcher
+
+    printExplanation
 
     f(mat).onComplete(res => {
       toDo.flatMap(t => res.toOption.map(t))
